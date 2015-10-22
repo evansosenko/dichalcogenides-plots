979cf7e6620009c78093407e1269072b731ebe04
Evan Sosenko <razorx@evansosenko.com>
2015-10-22 16:55:14 -0700

    Add WIP band plot

--- plots/energy.py
+++ plots/energy.py
@@ -1,9 +1,22 @@
+import itertools
+
+import numpy
 from dichalcogenides.dichalcogenide import Energy, Dichalcogenide
 
 from . import Plot
 
 def main():
-    plot_energy()
+    plot_bands('mos2')
 
-def plot_energy():
-    plt = Plot('energy')
+def plot_bands(material):
+    energy = Energy(Dichalcogenide(material, root='data')).e
+    plot = Plot('bands')
+    plt = plot.figure.add_subplot(111)
+
+    p = (-1, 1)
+    for x in itertools.product(p, p, p):
+        dk = x[1] * 1
+        k0 = x[1] * 1
+        k = numpy.linspace(-dk + k0, dk + k0, 100)
+        plt.plot(k, numpy.vectorize(lambda k: energy(k - k0, *x))(k))
+    plot.save()
--- plots/plot.py
+++ plots/plot.py
@@ -1,5 +1,7 @@
 import os
 
+import matplotlib.pyplot
+
 class Plot():
     def __init__(self, name, directory='build'):
         self.name = name
@@ -8,3 +10,31 @@ class Plot():
     @property
     def path(self):
         return os.path.join(self.directory, self.name)
+
+    @property
+    def figure_args(self):
+        return {}
+
+    @property
+    def figure(self):
+        """
+        Returns or creates a [`matplotlib.figure.Figure`][1] object.
+        Keyword arguments in `figure_args` are passed to [`matplotlib.pyplot.figure`][2].
+        [1]: http://matplotlib.org/api/figure_api.html#matplotlib.figure.Figure
+        [2]: http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.figure
+        """
+        if not hasattr(self, '_figure'):
+            self._figure = matplotlib.pyplot.figure(**self.figure_args)
+        return self._figure
+
+    def close(self):
+        """
+        Closes the [`matplotlib.figure.Figure`][1] object
+        with [`matplotlib.pyplot.close`][2].
+        [1]: http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.close
+        """
+        return matplotlib.pyplot.close(self.figure)
+
+    def save(self):
+        print(self.path)
+        self.figure.savefig(self.path)
