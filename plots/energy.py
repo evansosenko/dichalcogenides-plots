7ee9a002784573dbee8855be0c11f20a90cf45e1
Evan Sosenko <razorx@evansosenko.com>
2015-10-23 15:40:03 -0700

    First complete draft of band figure

--- plots/energy.py
+++ plots/energy.py
@@ -14,21 +14,85 @@ def plot_bands(material):
     uvb = UVBEnergy(dichalcogenide)
     energy = Energy(dichalcogenide)
 
-    # Create and configure plot object.
+    # Define additional parameters.
+    e = energy.e
+    dk = 0.4 # valley width
+    k0 = 0.5 # valley separation
+    t = 0.06 # text offset
+    tr = 0.01 # relative text offset
+
+    # Set center axes style.
+    axes_style = dict(
+        linewidth=0.5,
+        color='gray')
+
+    # Set dimension annotations style.
+    dimension_style = dict(
+        arrowstyle='|-|',
+        linewidth=0.5,
+        mutation_scale=5)
+
+    # Create and configure the plot object.
     plot = Plot('bands')
     plt = plot.figure.add_subplot(111)
     plt.axis('off')
 
-    # Add line for the chemical potential.
-    plt.axhline(uvb.μ)
+    # Add and label center axes.
+    plt.axhline(**axes_style)
+    plt.axvline(**axes_style)
+    plt.annotate('$E(k)$', (0.5, 1.05),
+                 xycoords='axes fraction',
+                 horizontalalignment='center',
+                 verticalalignment='center')
 
-    # Set additional parameters.
-    dk = 0.4 # valley width
-    k0 = 0.5 # valley separation
+    # Add chemical potential.
+    plt.axhline(uvb.μ, linestyle='--', color='black')
+    plt.annotate('$\\mu$', (-1.5 * k0, uvb.μ + t))
 
+    # Add band gap dimension.
+    plt.annotate('', (-k0, e(0, -1, 1, 1)), (-k0, e(0, 1, 1, 1)),
+                 arrowprops=dimension_style)
+    plt.annotate('$\\Delta$', (0.22, 0.5 + tr), xycoords='axes fraction')
+
+    # Add spin splitting dimension.
+    plt.annotate('', (-k0, e(0, -1, 1, 1)), (-k0, e(0, -1, 1, -1)),
+                 arrowprops=dimension_style)
+    plt.annotate('$2 \\lambda$', (-k0 - t, uvb.μ + t))
+
+    # Add valley labels.
+    plt.annotate('$\\tau = -$', (0, 0.5 + tr), xycoords='axes fraction')
+    plt.annotate('$\\tau = +$', (1, 0.5 + tr), xycoords='axes fraction',
+                 horizontalalignment='right')
+
+    # Add band labels.
+    plt.annotate('$n = -$', (0.5 + tr, 0.5 - tr), xycoords='axes fraction',
+                 verticalalignment='top',
+                 horizontalalignment='left')
+    plt.annotate('$n = +$', (0.5 + tr, 0.5 + tr), xycoords='axes fraction',
+                 verticalalignment='bottom',
+                 horizontalalignment='left')
+
+    # Add bands.
     p = (-1, 1)
     for x in itertools.product(p, p, p):
-        k = numpy.linspace(x[1] * (-dk + k0), x[1] * (dk + k0), 100)
-        plt.plot(k, numpy.vectorize(lambda k: energy.e(k - x[1] * k0, *x))(k))
+        k = numpy.linspace(x[1] * (k0 - dk), x[1] * (k0 + dk), 100)
+        fn = lambda k: e(k - x[1] * k0, *x)
 
+        # Plot band.
+        plt.plot(
+            k, numpy.vectorize(fn)(k),
+            color='black')
+
+        # Add spin annotation.
+        valign = ['bottom', 'top']
+        if x[0] == -1: valign.reverse()
+        valign.insert(0, None)
+        plt.annotate(spin(x[2]), (x[1] * (k0 + dk + t), fn(x[1] * (k0 + dk))),
+                 horizontalalignment='center',
+                 verticalalignment=valign[x[0] * x[1] * x[2]])
+
+    # Save figure.
     plot.save()
+
+def spin(s):
+    return [None, '$\\uparrow$', '$\\downarrow$'][s]
