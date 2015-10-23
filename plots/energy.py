2630800fa2b1630154397da380de8e80618df0ed
Evan Sosenko <razorx@evansosenko.com>
2015-10-23 11:18:59 -0700

    Add chemical potential to bands

--- plots/energy.py
+++ plots/energy.py
@@ -1,7 +1,7 @@
 import itertools
 
 import numpy
-from dichalcogenides.dichalcogenide import Energy, Dichalcogenide
+from dichalcogenides.dichalcogenide import Energy, UVBEnergy, Dichalcogenide
 
 from . import Plot
 
@@ -9,17 +9,26 @@ def main():
     plot_bands('wse2')
 
 def plot_bands(material):
-    energy = Energy(Dichalcogenide(material, root='data')).e
+    """Plot dichalcogenide band structure."""
+    dichalcogenide = Dichalcogenide(material, root='data')
+    uvb = UVBEnergy(dichalcogenide)
+    energy = Energy(dichalcogenide)
+
+    # Create and configure plot object.
     plot = Plot('bands')
     plt = plot.figure.add_subplot(111)
     plt.axis('off')
 
+    # Add line for the chemical potential.
+    plt.axhline(uvb.μ)
+
+    # Set additional parameters.
+    dk = 0.4 # valley width
+    k0 = 0.5 # valley separation
+
     p = (-1, 1)
     for x in itertools.product(p, p, p):
-        dk = 0.5
-        k0 = 0.5
-        s =  0.1
-        k = numpy.linspace(x[1] * (-dk + k0 + s), x[1] * (dk + k0 - s), 100)
-        plt.plot(k, numpy.vectorize(lambda k: energy(k - x[1] * k0, *x))(k))
+        k = numpy.linspace(x[1] * (-dk + k0), x[1] * (dk + k0), 100)
+        plt.plot(k, numpy.vectorize(lambda k: energy.e(k - x[1] * k0, *x))(k))
 
     plot.save()
