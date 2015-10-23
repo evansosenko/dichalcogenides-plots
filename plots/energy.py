4a9b31dec0d61e8efa228e3a04430f8fb72ff88c
Evan Sosenko <razorx@evansosenko.com>
2015-10-23 10:56:15 -0700

    Starting point for band figure

--- plots/energy.py
+++ plots/energy.py
@@ -6,17 +6,20 @@ from dichalcogenides.dichalcogenide import Energy, Dichalcogenide
 from . import Plot
 
 def main():
-    plot_bands('mos2')
+    plot_bands('wse2')
 
 def plot_bands(material):
     energy = Energy(Dichalcogenide(material, root='data')).e
     plot = Plot('bands')
     plt = plot.figure.add_subplot(111)
+    plt.axis('off')
 
     p = (-1, 1)
     for x in itertools.product(p, p, p):
-        dk = x[1] * 1
-        k0 = x[1] * 1
-        k = numpy.linspace(-dk + k0, dk + k0, 100)
-        plt.plot(k, numpy.vectorize(lambda k: energy(k - k0, *x))(k))
+        dk = 0.5
+        k0 = 0.5
+        s =  0.1
+        k = numpy.linspace(x[1] * (-dk + k0 + s), x[1] * (dk + k0 - s), 100)
+        plt.plot(k, numpy.vectorize(lambda k: energy(k - x[1] * k0, *x))(k))
+
     plot.save()
