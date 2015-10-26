2b1a8d083973ce4fc58faf660f741b9645c1729c
Evan Sosenko <razorx@evansosenko.com>
2015-10-26 16:15:01 -0700

    Add WIP optical plot

--- plots/optical.py
+++ plots/optical.py
@@ -1,15 +1,17 @@
 import numpy
 
 from dichalcogenides.dichalcogenide import Optical, UVBEnergy
+from dichalcogenides.superconductor import Induced
 
 from . import Plot
 
 def main():
-    PlotOptical('wse2').plot_all().save()
+    PlotOptical('wse2', 'induced').plot_all().save()
 
 class PlotOptical(Plot):
-    def __init__(self, material):
+    def __init__(self, material, system):
         self.material = material
+        self.system = system
         self.opts = dict(
             n=100 # linspace points
         )
@@ -24,7 +26,7 @@ class PlotOptical(Plot):
 
     def plot_all(self):
         """Create complete figure."""
-        self.plot_rate(1).plot_rate(-1)
+        self.plot_sc_p()
         return self
 
     def plot_rate(self, alpha):
@@ -35,5 +37,53 @@ class PlotOptical(Plot):
         fn = lambda xi: p(xi + uvb.μ)
         self.plot.plot(
             xi, numpy.vectorize(fn)(xi))
+
+        return self
+
+    def plot_p(self):
+        uvb = UVBEnergy(self.dichalcogenide)
+        p = lambda a: Optical(self.dichalcogenide, 1, a).p_circular
+        xi = numpy.linspace(*uvb.ξ_bounds, self.opts['n'])
+
+        pxi = lambda a, xi: p(a)(xi + uvb.μ)
+        fn = lambda xi: (pxi(1, xi) - pxi(-1, xi)) / (pxi(1, xi) + pxi(-1, xi))
+
+        self.plot.plot(
+            xi, numpy.vectorize(fn)(xi))
+
+        return self
+
+    def plot_sc_rate(self, alpha):
+        uvb = UVBEnergy(self.dichalcogenide)
+        p = Optical(self.dichalcogenide, 1, alpha).p_circular
+        sc = Induced(self.dichalcogenide)
+        xi = sc.ξ
+        dk = sc.Δk(0)
+        s = sc.trig('sin^2 β')
+
+        lk = numpy.linspace(*sc.λk_bounds(dk), self.opts['n'])
+
+        fn = lambda lk: s(dk, lk) * p(xi(dk, lk) + uvb.μ)
+        self.plot.plot(
+            lk, numpy.vectorize(fn)(lk))
+
+        return self
+
+    def plot_sc_p(self):
+        uvb = UVBEnergy(self.dichalcogenide)
+        sc = Induced(self.dichalcogenide)
+        xi = sc.ξ
+        dk = sc.Δk(0)
+        s = sc.trig('sin^2 β')
+
+        p = lambda a: Optical(self.dichalcogenide, 1, a).p_circular
+        psc = lambda a, lk: s(dk, lk) * p(a)(xi(dk, lk) + uvb.μ)
+
+        fn = lambda lk: (psc(1, lk) - psc(-1, lk)) / (psc(1, lk) + psc(-1, lk))
+
+        lk = numpy.linspace(*sc.λk_bounds(dk), self.opts['n'])
+
+        self.plot.plot(
+            lk, numpy.vectorize(fn)(lk))
 
         return self
