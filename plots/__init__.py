ec0727abe22f393a17293c8178282bb55ce86eb4
Evan Sosenko <razorx@evansosenko.com>
2016-05-16 20:07:02 -0700

    Remove topology plot

--- plots/__init__.py
+++ plots/__init__.py
@@ -2,11 +2,9 @@ from .version import __version__
 from .plot import Plot
 from .energy import PlotBands
 from .optical import PlotOptical
-from .topology import PlotBerry
 
 __all__ = [
     'Plot',
     'PlotBands',
     'PlotOptical',
-    'PlotBerry'
 ]
--- plots/__main__.py
+++ plots/__main__.py
@@ -1,6 +1,5 @@
-from . import energy, optical, topology
+from . import energy, optical
 
 if __name__ == '__main__':
     energy.main()
     optical.main()
-    topology.main()
