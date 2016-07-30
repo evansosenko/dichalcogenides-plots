f90124a98b7a9e66c845184bc86ddd5c2634fe56
Evan Sosenko <razorx@evansosenko.com>
2016-07-29 18:19:27 -0700

    Add plot for normal optical transitions

--- plots/optical.py
+++ plots/optical.py
@@ -8,28 +8,42 @@ from . import Plot
 
 def main():
     plot_optical('mose2', 'ws2', 'wse2')
+    plot_optical_bcs('mose2', 'ws2', 'wse2')
 
 def plot_optical(*materials):
-    plot = PlotOptical(None, 'induced')
+    plot = PlotOptical(None, 'induced', 'optical-transitions')
 
     legend = []
     for material in materials:
         plot.material = material
         legend.append(plot.dichalcogenide.material.name)
-        plot.plot_all()
+        plot.plot_p()
+
+    legend = ['$\\mathregular{' + l + '}$' for l in legend]
+    plot.plot[0].legend(legend)
+    plot.save()
+
+def plot_optical_bcs(*materials):
+    plot = PlotOptical(None, 'induced', 'optical-transitions-bcs')
+
+    legend = []
+    for material in materials:
+        plot.material = material
+        legend.append(plot.dichalcogenide.material.name)
+        plot.plot_p_sc()
 
     legend = ['$\\mathregular{' + l + '}$' for l in legend]
     plot.plot[0].legend(legend)
     plot.save()
 
 class PlotOptical(Plot):
-    def __init__(self, material, system):
+    def __init__(self, material, system, name):
         self.material = material
         self.system = system
         self.opts = dict(
             n=100 # linspace points
         )
-        super(self.__class__, self).__init__('optical-transitions')
+        super(self.__class__, self).__init__(name)
 
     @property
     def plot(self):
@@ -41,8 +55,6 @@ class PlotOptical(Plot):
                 self.figure.add_subplot(212)
             ]
             self.plot[0].axes.get_xaxis().set_visible(False)
-            self.plot[1].set_xlabel(
-                '$\\lambda_{\\mathbf{k}} / \\Delta_{\\mathbf{k}}$')
             head = '$\\left| \\left( c / ℏ \\right) P_'
             foot = '\\right|^2$ $\\left(\\mathregular{GeV}^2\\right)$'
             self.plot[0].set_ylabel(head + '+' + foot)
@@ -53,12 +65,38 @@ class PlotOptical(Plot):
             self.figure.subplots_adjust(hspace=0.1)
         return self._plot
 
-    def plot_all(self):
-        """Create complete figure."""
-        self.plot_sc_p()
+    def plot_p(self):
+        uvb = UpperValenceBand(self.dichalcogenide)
+        sc = Induced(self.dichalcogenide)
+        dk = sc.Δk(0)
+
+        p = Optical(self.dichalcogenide).p_circular
+        psc = lambda a, xi: p(xi + uvb.μ, 1, a)
+        fn_m = lambda xi: psc(-1, xi)
+        fn_p = lambda xi: psc(1, xi)
+
+        xi = numpy.linspace(-1, 0, self.opts['n'])
+
+        err = numpy.geterr()
+        numpy.seterr(invalid='ignore')
+
+        line = next(self.lines)
+        self.plot[0].plot(
+            xi, numpy.vectorize(fn_p)(xi),
+            line, color='black', linewidth=2)
+
+        self.plot[1].plot(
+            xi, numpy.vectorize(fn_m)(xi),
+            line, color='black', linewidth=2)
+
+        numpy.seterr(**err)
+
+        self.plot[1].set_xlabel(
+            '$E - \\mu$ $\\left(\\mathregular{eV} \\right)$')
+
         return self
 
-    def plot_sc_p(self):
+    def plot_p_sc(self):
         uvb = UpperValenceBand(self.dichalcogenide)
         sc = Induced(self.dichalcogenide)
         xi = sc.ξ
@@ -86,5 +124,8 @@ class PlotOptical(Plot):
             line, color='black', linewidth=2)
 
         numpy.seterr(**err)
+
+        self.plot[1].set_xlabel(
+            '$\\lambda_{\\mathbf{k}} / \\Delta_{\\mathbf{k}}$')
 
         return self
